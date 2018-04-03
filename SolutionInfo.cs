94fa277a4efd5ad4428aef08a79007ad9bf917a2
CodeCasterNL <github@codecaster.nl>
2018-04-03 14:53:39 +0200

    Up version to 1.1.0, unify assembly versions through SolutionInfo.cs.

---
+++ SolutionInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("CodeCaster")]
+[assembly: AssemblyCopyright("Copyright © CodeCaster 2016-2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("en-US")]
+
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
