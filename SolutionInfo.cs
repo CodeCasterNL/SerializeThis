93cc94c0175acb50ada4340feb75d967f689e379
CodeCasterNL <github@codecaster.nl>
2018-06-23 11:57:57 +0200

    Remove culture, this caused assemblies to be probed in a non-existent "en-US" directory

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -3,7 +3,6 @@
 [assembly: AssemblyCompany("CodeCaster")]
 [assembly: AssemblyCopyright("Copyright © CodeCaster 2016-2018")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("en-US")]
 
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
